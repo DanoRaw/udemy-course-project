6143677d3496f5650b63bb004835a5d1ba914887
Dano Rawlins <dano.hyde@gmail.com>
2020-12-08 17:33:30 -0600

    Shopping List Add Button Working

--- udemy-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected= new EventEmitter<Recipe>();
   recipes: Recipe[]= [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://storage.needpix.com/rsynced_images/food-1459693_1280.jpg')
   ];
@@ -14,5 +15,7 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
-
+onRecipeSelected(recipe: Recipe) {
+  this.recipeWasSelected.emit(recipe);
+}
 }
